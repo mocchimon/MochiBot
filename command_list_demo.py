487ac93d16106bfa65f1bba675d778c81a76e351
Henry Nguyen <mocchimon@users.noreply.github.com>
2018-09-27 15:19:23 -0700

    Add files via upload

--- command_list_demo.py
+++ command_list_demo.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='!')
 
 @bot.command(pass_context=True)
-async def add(ctx, a: int, b: int):
+async def plus(ctx, a: int, b: int):
     await bot.say(a + b)
 
 @bot.command(pass_context=True)
@@ -13,32 +13,38 @@ async def multiply(ctx, a: int, b: int):
 
 @bot.command(pass_context=True)
 async def greet(ctx):
-    await bot.say(":smiley: :wave: Hello, there!")
+    await bot.say(":smiley: :wave: Hello, there!" + ctx.message.author.mention)
 
 @bot.command(pass_context=True)
 async def cat(ctx):
     await bot.say("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
 
 @bot.command(pass_context=True)
+async def roles(ctx):
+    result = [role.name for role in ctx.message.server.roles if not role.is_everyone]
+    await bot.say(result)
+
+@bot.command(pass_context=True)
 async def info(ctx):
     embed = discord.Embed(title="Mochi bot", description="Mochiest bot there is ever.", color=0xeee657)
     # give info about you here
     embed.add_field(name="Author", value="<Mochi#6689>")
-    # give users a link to invite thsi bot to their server
-    embed.add_field(name="Invite", value="[Invite link](<insert your OAuth invitation link here>)")
+    # give users a link to invite this bot to their server
+    embed.add_field(name="Invite", value="[https://discordapp.com/oauth2/authorize?&client_id=493648422671351819&scope=bot&permissions=8]")
     await bot.say(embed=embed)
 
 bot.remove_command('help')
 
 @bot.command(pass_context=True)
 async def help(ctx):
-    embed = discord.Embed(title="nice bot", description="A Very Nice bot. List of commands are:", color=0xeee657)
+    embed = discord.Embed(title="nice bot", description="A Very mochi bot ~chi. List of commands are:", color=0xeee657)
     embed.add_field(name="!add X Y", value="Gives the addition of **X** and **Y**", inline=False)
     embed.add_field(name="!multiply X Y", value="Gives the multiplication of **X** and **Y**", inline=False)
     embed.add_field(name="!greet", value="Gives a nice greet message", inline=False)
     embed.add_field(name="!cat", value="Gives a cute cat gif to lighten up the mood.", inline=False)
     embed.add_field(name="!info", value="Gives a little info about the bot", inline=False)
     embed.add_field(name="!help", value="Gives this message", inline=False)
+    embed.add_field(name="!roles", value="Set your role", inline=False)
     await bot.say(embed=embed)
 
 def on_command_error(self, error, ctx):
